8d6d90b568524902948d00ec30a045d73cfb3979
Sebastian M <sebmueller1999@gmail.com>
2022-12-03 21:46:52 +0100

    added flutter formatting

--- lua/sebastian/plugins/lsp/null-ls.lua
+++ lua/sebastian/plugins/lsp/null-ls.lua
@@ -18,6 +18,7 @@ null_ls.setup({
 		--  to disable file types use
 		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
 		formatting.prettier, -- js/ts formatter
+		formatting.dart_format,
 		formatting.stylua, -- lua formatter
 		formatting.autopep8, --python formatter
 		diagnostics.eslint_d.with({ -- js/ts linter
