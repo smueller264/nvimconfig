672f140537f37b2112531dcef9b617a729c2f3a8
Sebastian M <sebmueller1999@gmail.com>
2023-01-31 17:53:58 +0100

    Revert "added markdown preview"
    This reverts commit bba58389c9396bcf5678dd978240d72828dc81f9.

--- lua/sebastian/plugins-setup.lua
+++ lua/sebastian/plugins-setup.lua
@@ -108,15 +108,6 @@ return packer.startup(function(use)
 	-- Code Diagnostics Window
 	use({ "folke/trouble.nvim", requires = "kyazdani42/nvim-web-devicons" })
 
-	-- Markdown Preview
-	-- install without yarn or npm
-	use({
-		"iamcco/markdown-preview.nvim",
-		run = function()
-			vim.fn["mkdp#util#install"]()
-		end,
-	})
-
 	if packer_bootstrap then
 		require("packer").sync()
 	end
--- lua/sebastian/plugins/markdown-preview.lua
+++
@@ -1,8 +0,0 @@
--- import gitsigns plugin safely
-local setup, markdown = pcall(require, "markdown-preview")
-if not setup then
-	return
-end
-
--- configure/enable gitsigns
-markdown.setup()
